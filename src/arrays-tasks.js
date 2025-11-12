03bfbe122555c0cab98a97b4d2f06d1d1092541e
Yuriy Li <0yuriyli@gmail.com>
2025-11-13 02:01:35 +0300

    feat: add solution for getHexRGBValues()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -475,8 +475,11 @@ function getIndicesOfOddNumbers(numbers) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map((n) => {
+    const hex = n.toString(16).toUpperCase().padStart(6, '0');
+    return `#${hex}`;
+  });
 }
 
 /**
