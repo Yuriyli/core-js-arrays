9a7ed35ad18f83cca4ccee3735630dfe0e49a856
Yuriy Li <0yuriyli@gmail.com>
2025-11-12 09:21:56 +0300

    feat: add solution for getIntervalArray()

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,12 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  const length = end - start + 1;
+  const arr = new Array(length);
+  return Array.from(arr, (a, i) => {
+    return start + i;
+  });
 }
 
 /**
